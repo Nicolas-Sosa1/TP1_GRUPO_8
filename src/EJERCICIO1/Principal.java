85f3052376607794ab17d6a2ddfc1e671d06fb18
pedropursall <pedropursall@gmail.com>
2025-08-20 20:38:11 -0300

    Clase Persona agregada, constructores de la clase Persona agregados

---
+++ src/EJERCICIO1/Principal.java
@@ -0,0 +1,25 @@
+package EJERCICIO1;
+
+import java.time.LocalDate;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		
+		//Empleados constructor vacío
+		
+		Empleado empleado1 = new Empleado();
+		Empleado empleado2 = new Empleado();
+		Empleado empleado3 = new Empleado();
+		
+		//Empleados constructor con parámetros
+		
+		Empleado empleado4 = new Empleado("dni4", "nombre4", "apellido4", LocalDate.of(2000, 4,4), 
+				"genero4", "direccion4", "telefono4", "email4", "puesto4");
+		
+		Empleado empleado5 = new Empleado("dni5", "nombre5", "apellido5", LocalDate.of(2000, 5,5), 
+				"genero5", "direccion5", "telefono5", "email5", "puesto5");
+				
+	}
+}
